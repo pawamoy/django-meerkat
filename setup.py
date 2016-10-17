faab3e01d25303457eb05adab911850d0cbaa3e0
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-10-17 16:42:08 +0200

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-meerkat',
-    version='0.1.0',
+    version='0.1.1',
     license='MPL 2.0',
     description='Security audit tool for Django sites',
     long_description='%s\n%s' % (
