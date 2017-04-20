6c72c4e4e06a1a01b0bd276aa6930960defbc38c
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-04-20 21:20:16 +0200

    Update cookiecutter branch

--- tests/test_meerkat.py
+++ tests/test_meerkat.py
@@ -3,7 +3,6 @@
 """Main test script."""
 
 
-
 from django.test import TestCase
 
 import meerkat
@@ -14,12 +13,12 @@ class MainTestCase(TestCase):
 
     def setUp(self):
         """Setup method."""
-        pass
+        self.package = meerkat
 
     def test_main(self):
         """Main test method."""
-        assert meerkat
+        assert self.package
 
     def tearDown(self):
         """Tear down method."""
-        pass
+        del self.package
