00feabd184d05f74d2fc89ad2728261ba3629c44
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-02 19:37:24 +0200

    Update appsettings version, monkey patch progress bar for django18, update migrations

--- src/meerkat/logs/models.py
+++ src/meerkat/logs/models.py
@@ -14,7 +14,6 @@ which is the difficulty here. Work is in progress.
 import datetime
 import sys
 
-from django.core.serializers.base import ProgressBar
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
@@ -24,6 +23,31 @@ from ..utils.file import count_lines, follow
 from ..utils.ip_info import ip_api_handler, ip_info_handler
 from ..utils.thread import StoppableThread
 from .parsers import NginXAccessLogParser
+
+try:
+    from django.core.serializers.base import ProgressBar
+except ImportError:
+    class ProgressBar(object):
+        progress_width = 75
+
+        def __init__(self, output, total_count):
+            self.output = output
+            self.total_count = total_count
+            self.prev_done = 0
+
+        def update(self, count):
+            if not self.output:
+                return
+            perc = count * 100 // self.total_count
+            done = perc * self.progress_width // 100
+            if self.prev_done >= done:
+                return
+            self.prev_done = done
+            cr = '' if self.total_count == 1 else '\r'
+            self.output.write(cr + '[' + '.' * done + ' ' * (self.progress_width - done) + ']')  # noqa
+            if done == self.progress_width:
+                self.output.write('\n')
+            self.output.flush()
 
 
 # Define what information retrievable from the logs are pertinent
