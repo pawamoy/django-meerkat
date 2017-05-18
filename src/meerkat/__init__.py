b5d8423dad0ea33db3ba13eb8589d7c7361b5f44
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2017-05-18 13:48:11 +0200

    Bump version: 0.2.2 → 0.2.3

--- src/meerkat/__init__.py
+++ src/meerkat/__init__.py
@@ -7,6 +7,6 @@ Django Meerkat provides utils to monitor activity through the server logs,
 but also to run security and architecture analysis checks.
 """
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 default_app_config = 'meerkat.apps.MeerkatConfig'
